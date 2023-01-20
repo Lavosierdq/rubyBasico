e1e84f9e94753e75cdbd1f52d88fe2a099671cf2
Lavosierdq <lavosierbarbosa@gmail.com>
2023-01-19 23:22:23 -0300

    exercicos de lacos em ruby

---
+++ exercicio/exercicioLacos/lacosExercicios3.rb
@@ -0,0 +1,38 @@
+=begin 6. Faça um programa que receba do usuário um vetor com 10 posições. Em seguida deverá
+ser impresso o maior e o menor elemento do vetor.
+=end
+
+
+
+vetor = []
+vetor2 =[]
+maior = 0
+menor = 9999
+
+
+x = 0
+loop do
+  puts 'digite os numeros'
+  vet = gets.to_i
+  vetor.append(vet)
+  x+=1 
+  break if x > 9
+end
+
+x = 0
+
+loop do 
+  break if x > 9
+  if vetor[x] >= maior
+    maior = vetor [x]
+
+  else vetor[x] <= menor
+    menor = vetor[x]
+  end
+  x+=1
+end
+
+puts "o numero maior #{ maior} e o menor numero #{menor}"
+
+
+
