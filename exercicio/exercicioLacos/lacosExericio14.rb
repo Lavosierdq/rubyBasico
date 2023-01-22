c60030c548483d82994f64a5709b184af12e3b8e
Lavosierdq <lavosierbarbosa@gmail.com>
2023-01-21 23:48:49 -0300

    comitando lacosExercicio14.rb

---
+++ exercicio/exercicioLacos/lacosExericio14.rb
@@ -0,0 +1,22 @@
+=begin 14. Faça um programa que leia um vetor de 10 posições e verifique se existem valores iguais
+e os escreva na tela.
+=end
+
+vetor = []
+
+for i in (0..9) do
+  puts 'digite os numeros a seguir'
+  vet = gets.to_i
+  vetor << vet
+end
+
+
+for n in (0..9) do
+
+  while j < n+1
+
+  j+=1
+  end
+
+end
+
