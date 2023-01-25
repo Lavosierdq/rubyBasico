61c8776f2df5f73778ebc4ea6ff03462140a1888
Lavosierdq <lavosierbarbosa@gmail.com>
2023-01-24 23:27:13 -0300

    estudo de recursividade

---
+++ recursividade.rb
@@ -0,0 +1,31 @@
+#exercicio de recursividade.
+
+def fatorial (x)
+
+  if x == 0
+    resultado = 1
+  else
+    resultado = x*fatorial(x-1)
+  end
+
+  return resultado
+end
+
+
+
+resultado = 0
+puts ' digite um numero'
+numero = gets.to_i
+
+resultado =  fatorial(numero)
+
+puts "o fatoria e #{resultado}"
+
+=begin
+
+a função recursiva para retornar o resultado de fatorial
+obs: linguagem intepretada a função tem que ser lida primeiro
+diferente da linguagem compilada, caso contrário errro.
+
+=end
+
