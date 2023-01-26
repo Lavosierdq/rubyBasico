97d63a7e04379d7154b13a5cccac2c399633eb0e
Lavosierdq <lavosierbarbosa@gmail.com>
2023-01-26 00:41:51 -0300

    exercicio de busca linear feito

--- linearSearch.rb
+++ linearSearch.rb
@@ -12,27 +12,29 @@ Input: arr[] = {10, 20, 80, 30, 60, 50,110, 100, 130, 170}, x = 175;
 Output: -1
 Explanation: Element x is not present in arr[].
 =end
-def linear(escolha,x)
+
+def linear(x)
   vetor  = [10,20,80,230,60,50,110,100,130,170]
-  vet  = gets.to_i
   for i in (0..8) do
-    if vetor[i] == vet
-
-      return "vetor[i]"
+    if vetor[i] == x
+      return "o valor da busca linear #{vetor[i]} e o elemento esta no indice #{i}"
       break;
-
     end 
 
   end
+  return -1 
 
-  if  i == 9
-    return -1
-  end
 end
 
 
-pust " digite o numero para a busca linear"
+puts " digite o numero para a busca linear"
 num = gets.to_i
 
-puts linear(num,x)
+if linear(num) == -1
+  puts " o elemento x não está presente no arr[]"
+else
+
+  puts linear(num)
+
+end
 
