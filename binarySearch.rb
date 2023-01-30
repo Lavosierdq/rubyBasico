2ca0d2287b1c2d803a81b916eb9012badce4c35d
Lavosierdq <lavosierbarbosa@gmail.com>
2023-01-29 23:07:51 -0300

    comitando a busca linear

--- binarySearch.rb
+++ binarySearch.rb
@@ -4,42 +4,33 @@ intput: arr[]={10,20,80,30,60,50,110,100,130,170}, x = 175
 output:-1
 Explanation :Element x is not present in arr[]
 =end
-
-def binario(arr, x)
-
-  vet = [] << arr
-  indiceMedia = arr.length/2
-  indiceMenor = 0
-  indiceMaior = vet.length
+def buscaBinaria (num,vetor,maior,menor)
 
 
-  for indiceMenor in indiceMaior do
-
-    if vet[indiceMedia] == x
-
-      return indice media
-
-    elsif vet[media] > x
-
-      vet.length = indiceMedia - 1 
-
+  while menor <= maior
+  meio  = (menor + maior) / 2
+    if vetor [meio] == num
+      return meio
+    elsif vetor[meio] < num
+      menor = meio + 1
     else
-
-      indiceMenor = indiceMedia+1
-
-    end 
-
+      maior = meio - 1 
+    end
   end
-  return -1
 
+  if menor >  maior
+    return -1
+  end
 end
 
-
- 'digite o numero para a busca binaria'
+puts 'digite um numero'
 num = gets.to_i
+vetor = [5,10,27,35,55,68,77,99,110,113]
+maior = vetor.length - 1
+menor = 0
 
-vetor = [10,20,80,30,60,50,110,130,170]
-
-puts binario(vetor,num)
-
-
+if buscaBinaria(num,vetor,maior,menor) == -1
+  puts "numero não encontrado #{buscaBinaria(num,vetor,maior,menor)}"
+else
+  puts "indice:#{buscaBinaria(num,vetor,maior,menor)} e valor:#{vetor[buscaBinaria(num,vetor,maior,menor)]}"
+end
