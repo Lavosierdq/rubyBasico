d3343b43b64782466598457dabceb596de1a5185
Lavosierdq <lavosierbarbosa@gmail.com>
2023-01-31 22:47:41 -0300

    bubbleSort.rb terminado

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -1,31 +1,28 @@
 #Bubble Sort Algorithm
 
 arr = [5,1,4,2,8]
-n = arr.length
-puts "o valor de n #{n}"
-buscaBolha(arr)
+n = arr.length - 1
 
+puts buscaBolha(arr,n)
 
-
-BEGIN{ 
-
-  def buscaBolha(arr)
+BEGIN {
+  def buscaBolha(arr,n)
     i = 0
     j = 0
-    while i < arr.length-1
-      i = i+1
-      while j < i - arr.length-1
+
+    while i < (n-1)
+
+      while j < (i-n-1)
 
         if arr[j] > arr[j+1]
           aux = arr[j]
           arr[j] = arr[j+1]
           arr[j+1] = aux
         end
-      end
-     end
-
+        j = j+1
+      end 
+      i = i+1
+    end
     return arr
   end
 }
-
-puts "o valor do vetor ordenado #{buscaBolha(arr)}"
