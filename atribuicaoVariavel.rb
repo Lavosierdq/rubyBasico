8de31d94f373cce55f2557b5edd9067624a53373
Lavosierdq <lavosierbarbosa@gmail.com>
2023-02-06 22:55:45 -0300

    corrigindo exercicio de atribuição

--- atribuicaoVariavel.rb
+++ atribuicaoVariavel.rb
@@ -3,15 +3,19 @@
 variavel = nil
 variavel = "algum valor" if variavel.nil?
 
+puts " o valor de variavel #{variavel}"
+
 #nulo em ruby é avaliado como falso então..
 
 variavel = nil
 
 variavel = "algum valor" if !variavel
-
+puts " o segundo valor da variavel #{variavel}"
 # a variavel recebera o valor se ela for nula
 variavel = nil
 variavel ||= "valor"
+
+puts "o terceiro valor da variavel #{variavel}"
 
 num = 10
 puts "#{num}"
