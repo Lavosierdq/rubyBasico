98fb1d0185467a69309d04cdaf42c994763b4cef
Lavosierdq <lavosierbarbosa@gmail.com>
2023-02-14 21:17:53 -0300

    comitando exercicio de algoritimo guloso

---
+++ algoritmoGuloso2.rb
@@ -0,0 +1,23 @@
+=begin exercicio de algoritmo guloso 
+=end
+
+inicio = [ 1,3,0,5,8,5]
+fim = [ 2,4,6,7,9,9]
+n = 4
+
+puts  entradaDadosExercicio(inicio,fim,n)
+
+BEGIN{
+  def entradaDadosExercicio(inicio,fim,n)
+    i = 0
+    j = 1
+    print "#{i} "
+    while  j <= n do
+      if inicio[j] >= fim[i]
+      print "#{j} "
+      i = j
+      end
+      j = j+1
+    end
+  end
+}
