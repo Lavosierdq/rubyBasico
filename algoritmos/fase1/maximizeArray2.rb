d629e1ee28d86c33d2771825027594d6133ff11d
Lavosierdq <lavosierbarbosa@gmail.com>
2023-02-25 22:53:13 -0300

    corrigindo o dever de maximizeArray2.rb

--- algoritmos/fase1/maximizeArray2.rb
+++ algoritmos/fase1/maximizeArray2.rb
@@ -7,19 +7,24 @@ operação de forma que após K operações, a soma do array seja máxima?
 
 def maximumArray(vet,k,n)
 
- #fazendo a quantidade de interações 
+  #fazendo a quantidade de interações 
   i = 0
   j = 0
+  #return puts 'o valor de vet'
   while i < k
-    menor = 98541215441 
+    menor = 9999
     index = -1
 
     while j < n
-      if vetor[j] < menor
-        menor = vetor[j]
+     #return "o valor de j no inicio #{j}"
+      if vet[j] < menor
+        menor = vet[j]
         index = j
+        #return " o valor de menor #{menor}"
       end
-      j=+1
+
+      j+=1 
+      #return " o valor de j #{j} e o valor de n: #{n}"
     end
 
     if menor == 0
@@ -28,14 +33,22 @@ def maximumArray(vet,k,n)
 
     vet[index] = -vet[index] 
     i+=1
+    return " o valor de i #{i}"
   end
-  
+
+  i = 0
+  soma = 0
+
+  while i < n
+    soma += vetor[i] 
+    i += 1
+  end
+
+  #return soma
 end
 
-
-array = [9,8,8,5]
+vet = [9,8,8,5]
 k = 3
-interacoes = 3
-n = array.length-1
-puts n
-puts maximumArray(array,k,n)
+n = vet.length-1
+puts vet 
+puts maximumArray(vet,k,n)
