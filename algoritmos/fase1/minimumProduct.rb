751f183250ffd031522c3e208046f255feec268e
Lavosierdq <lavosierbarbosa@gmail.com>
2023-02-26 23:55:56 -0300

    exercicio minimum produt

---
+++ algoritmos/fase1/minimumProduct.rb
@@ -0,0 +1,25 @@
+=begin exercio de
+Minimum product subset of an array
+https://www.geeksforgeeks.org/minimum-product-subset-array/
+=end
+
+vet = [-1,-1,-2,4,3]
+puts miniumProduct(vet)
+
+BEGIN {
+ def miniumProduct(vet)
+
+  i = 0
+  j = 0
+  for i in vet.each do 
+    if i < 0
+      numeroNegativo+=1
+    else
+      numeroPositivo+=1
+  end
+
+  return p "o valor de #{j}"
+end
+}
+
+
