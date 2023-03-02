321ed12ae201c1a62bde7211d90e1c6f2bfc4a59
Lavosierdq <lavosierbarbosa@gmail.com>
2023-03-02 10:21:32 -0300

    terminando o exercicio

---
+++ algoritmos/fase1/exemplo.rb
@@ -0,0 +1,12 @@
+#exemplo de ruby
+
+
+vetor = []
+i = 
+
+for i in 0..5 do
+
+ vetor[] = i 
+end
+
+puts vetor
--- algoritmos/fase1/minimumProduct.rb
+++ algoritmos/fase1/minimumProduct.rb
@@ -4,47 +4,42 @@ https://www.geeksforgeeks.org/minimum-product-subset-array/
 =end
 
 vet = [-1,-1,-2,4,3]
-p miniumProduct(vet)
+n = vet.length
+puts "o valor de n #{n}"
+p miniProduct(n,vet)
+
+
 BEGIN{
- def miniumProduct(vet)
+ def miniProduct(n,vet)
+
   i = 0
   j = 0
   numNegativo = 0
   numZero = 0
   numPositivo = 0
-  vetNegativo []
-  vetPositivo []
+  vetNegativo = []
+  vetPositivo = []
 
-  if numVet == 1
-    return 0
+  if ( n == 1)
+    return vet[0]
   end
-
-  for i in vet.each do 
-
-    if i < 0
-      numNegativo += 1
-      vetNegativo[i+=1]
-      
-    elsif i == 0
-      numZero+=1
+  while i < vet.length - 1 
+    
+    if vet[i] < 0
+      vetNegativo[i] = vet[i]
+    elsif
+      vet[i] == 0
+      numZero += 1
     else
-      numPositivo+=1
-    end
-  end 
-  
-  i = 0
-  j = 0
-  maiorNegativo =0
-
-  while i < vetNegativo.length-1
-    while j < i
-      if vetNegativo[j] > vetNegativo[i+1]
-        maiorNegativo = vetNegativo[j]
-      end
-      j+=1
+      numPositivo +=1
     end
     i+=1
   end
+  return vetNegativo
+  i = 0
+  j = 0
+  #maiorNegativo = -999999999999999999
+
  end 
 }
 
