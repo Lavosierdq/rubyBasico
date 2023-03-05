8209928e801350b67e00213687539a6055b5e858
Lavosierdq <lavosierbarbosa@gmail.com>
2023-03-05 01:38:20 -0300

    fazendo exercicios

--- algoritmos/fase1/minimumProduct.rb
+++ algoritmos/fase1/minimumProduct.rb
@@ -9,45 +9,74 @@ p miniProduct(n,vet)
 
 
 BEGIN{
- def miniProduct(n,vet)
+  def miniProduct(n,vet)
 
-  i = 0
-  j = 0
-  numNegativo = 0
-  numZero = 0
-  numPositivo = 0
-  vetNegativo = []
-  vetPositivo = []
+    i = 0
+    j = 0
+    numNegativo = 0
+    numZero = 0
+    numPositivo = 0
+    vetNegativo = []
+    vetPositivo = []
 
-  if ( n == 1)
-    return vet[0]
-  end
-  while i < vet.length-1
-    
-    if vet[i] < 0
-      vetNegativo[i] = vet[i]
-      numNegativo  = numNegativo + 1
-    elsif
-      vet[i] == 0
-      numZero += 1
+    if ( n == 1)
+      return vet[0]
+    end
 
+    while i < vet.length-1
+
+      if vet[i] < 0
+        vetNegativo[i] = vet[i]
+        numNegativo  = numNegativo + 1
+      elsif vet[i] == 0
+        numZero += 1
+      else
+        numPositivo +=1
+      end
+      i+=1
+    end
+    i = 0
+    j = 0
+    menorPositivo = []
+    maior = 999999999999999999
+
+    while i < vet.length
+      if  vet[i] > 0
+        if vet[i] < maior
+          maior = vet[i]
+          menorPositivo[j] = maior
+        end
+      end
+      i+=1
+    end
+    return puts " o valor menorPositivo #{menorPositivo[j]}"
+
+    maiorNegativo = -999999999999999999 
+    while i < vetNegativo.length-1
+      if maiorNegativo <  vetNegativo[i]
+        maiorNegativo = vetNegativo[i]
+      end
+      i+=1
+    end
+
+    i = 0
+    j = 0
+    somaVet = 0
+    resultado = 0
+
+    if numNegativo%2 == 0 && numZero == 0
+      while i < vetNegativo.length-1
+        somaVet+=vet[i]
+        i+=1
+      end 
+      resultado = somaVet/maiorNegativo 
+    elsif numNegativo%2 != 0 && numZero == 0
+      somaVet
+    elsif numPositivo > 0 && numZero > 0
+      return 0
     else
-      numPositivo +=1
+      
     end
-    i+=1
-  end
-  i = 0
-  maiorNegativo = -999999999999999999 
-  while i < vetNegativo.length-1
-    if maiorNegativo <  vetNegativo[i]
-      maiorNegativo = vetNegativo[i]
-    end
-     i+=1
-  end
 
- return puts "o valor de i : #{i} e  num negativo #{numNegativo}, num positivo #{numPositivo}, num maior negativo #{maiorNegativo}"
-  j = 0
-  #maiorNegativo = -999999999999999999
-
- end 
+  end 
 }
