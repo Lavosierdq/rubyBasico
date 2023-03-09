6e6b703629b38db2fcbe7afd228eb9aa77cb3ed7
Lavosierdq <lavosierbarbosa@gmail.com>
2023-03-09 01:31:23 -0300

    continuando estudo de classes em ruby

--- rubyP.O.O/aulaClasses/exercicioClasse/carro/Carro3.rb
+++ rubyP.O.O/aulaClasses/exercicioClasse/carro/Carro3.rb
@@ -1,12 +1,15 @@
 class Carro3
-attr_accessor :marca,:modelo,:numPassageiros,:capCombustivel,:conCombustivel
+  attr_accessor :marca,:modelo,:numPassageiros,:capCombustivel,:conCombustivel
 
-def exibirAutonomia
-   "A autonomia do carro é: #{capCombustivel*conCombustivel}"
-end
-def obterAutonomia
- print 'Obter autonomia foi chamado '
-  capCombustivel * conCombustivel
-end
+  def exibirAutonomia
+    "A autonomia do carro é: #{capCombustivel*conCombustivel}"
+  end
+  def obterAutonomia
+    print 'Obter autonomia foi chamado '
+    capCombustivel * conCombustivel
+  end
+  def calcularCombustivel (km) #dentro da classe se chama passar parâmetro
+    qtdCombustivel  = km * capCombustivel
+  end
 
 end
--- rubyP.O.O/aulaClasses/exercicioClasse/carro/TesteCarro3.rb
+++ rubyP.O.O/aulaClasses/exercicioClasse/carro/TesteCarro3.rb
@@ -6,4 +6,6 @@ class TesteCarro3
   carro.conCombustivel = 70
   puts carro.exibirAutonomia
   puts carro.obterAutonomia
+  
+  puts carro.calcularCombustivel (15) #utilizando a classe se chama argumento
 end
