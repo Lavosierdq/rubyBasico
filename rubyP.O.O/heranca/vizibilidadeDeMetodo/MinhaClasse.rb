8f6efeafaf4949d038fa0b99fba65bc264005ca1
Lavosierdq <lavosierbarbosa@gmail.com>
2023-03-15 23:52:26 -0300

    exercicio de herança

---
+++ rubyP.O.O/heranca/vizibilidadeDeMetodo/MinhaClasse.rb
@@ -0,0 +1,13 @@
+class MinhaClasse
+  def m1
+    puts "método 1"
+  end
+  private # deixando método abaixo dessa linha em privado
+  def m2
+    puts "metodo 2"
+  end
+
+  def m3
+    puts "metodo 3"
+  end
+end
