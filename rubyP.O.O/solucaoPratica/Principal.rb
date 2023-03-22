5794b274e993b7c2e3667b35f4dcce3d3fd9d0ed
Lavosierdq <lavosierbarbosa@gmail.com>
2023-03-21 23:32:52 -0300

    exercicio de solução pratica

---
+++ rubyP.O.O/solucaoPratica/Principal.rb
@@ -0,0 +1,8 @@
+# arquivo main para executar
+require '../solucaoPratica/contaBancaria'
+
+conta_cadu = ContaBancaria.new('cadu', 100)
+conta_pessoa2 = ContaBancaria.new('pessoa2', 200)
+conta_cadu.transferir(conta_pessoa2, 50)
+p conta_cadu.saldo
+p conta_pessoa2.saldo
