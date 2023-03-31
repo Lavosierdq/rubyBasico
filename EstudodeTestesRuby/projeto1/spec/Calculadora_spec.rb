ac50c014c680b9d0bf8929031bb5d370cf56f784
Lavosierdq <lavosierbarbosa@gmail.com>
2023-03-30 23:38:50 -0300

    estudando teste com rspec

--- EstudodeTestesRuby/projeto1/spec/Calculadora_spec.rb
+++ EstudodeTestesRuby/projeto1/spec/Calculadora_spec.rb
@@ -1,8 +1,30 @@
 require '../lib/Calculadora'
 require '../spec/spec_helper'
+
 describe Calculadora do
   context 'quando for uma menssagem de teste' do
-    calculadora = Calculadora.new
-    expect(calculadora.nomeTeste2).to eq('vamos testar')
+    it 'e se receber uma menssagem' do
+      calculadora = Calculadora.new
+      calculadora.setNum1(10)
+      calculadora.setNum2(20)
+      expect(calculadora.nomeTeste2).to eq('vamos testar')
+      expect(calculadora.Soma).to eq(30)
+    end
+  end
+  context ' quando o valor for trocado' do
+    it ' se receber valores de 40 e 50' do
+      calculadora = Calculadora.new
+      calculadora.setNum1(40)
+      calculadora.setNum2(50)
+      expect(calculadora.Soma).to eq(90)
+    end
+  end
+  context ' se o usuário colocar uma letra' do
+    it ' retorna um erro' do
+      calculadora = Calculadora.new
+      calculadora.setNum1('valor')
+      calculadora.setNum2(30)
+      expect(calculadora.Soma).to eq(30)
+    end
   end
 end
--- algoritmos/fase1/findTree.rb
+++ algoritmos/fase1/findTree.rb
@@ -1,36 +1,28 @@
-=begin
-https://www.geeksforgeeks.org/find-the-largest-three-elements-in-an-array/
-Find the largest three distinct elements in an array
-=end
+# rubocop:disable all
+# https://www.geeksforgeeks.org/find-the-largest-three-elements-in-an-array/
+# Find the largest three distinct elements in an array
 
-vetor = [10,4,3,50,23,90]
-n = vetor.length -1
+vetor = [10, 4, 3, 50, 23, 90]
+n = vetor.length(-1)
+i = 0
+j = 0
+maior = 0
+aux = 0
+BEGIN{
+   while i < n
+    j = 0  
+    while j < i + 1
 
-Begin{
+      if vetor[j] > vetor[i + 1]
+        aux = vetor[j]
+        vetor[j] = vetor[i + 1]
+        vetor[i + 1] = aux
 
-  i = 0
-  j = 0
-  maior = 0
-  aux = 0
-  while i < n 
+      end
+      j += 1
 
-     while j < i+1
-
-       if vetor[j] > vetor[i+1]
-         aux = vetor[j]
-         vetor[j] = vetor[i+1]
-         vetor[i+1] = aux
-          j+=1
-         end
-       
-       end
-       i+=1
-    
+    end
+    i += 1
   end
-  
-
-
 
 }
-
-
