d14aebd8f12aa05a59a0832f12fa3710ea8cd17c
Lavosierdq <lavosierbarbosa@gmail.com>
2023-04-04 21:41:17 -0300

    terminando algoritmo

--- algoritmos/fase1/reversal.rb
+++ algoritmos/fase1/reversal.rb
@@ -5,15 +5,19 @@
 vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 p vetor
 n = vetor.length - 1
-j = 0 
+k = 0 
 aux = 0
-aux2= 0
-k = 0
-while j < n
-  while k < 1
-    aux = vetor[n]
-    aux2 = vetor[j+1]
-    vetor[n] = vetor[j]
-    vetor[j+1] = vetor[j]
+i = 0
+while i < n
+  k = 0
+  while k < 3
+    aux = vetor[i]
+    vetor[i] = vetor[n]
+    vetor[n] = aux 
+    k+=1
   end
+  i+=1
+  n-=1
 end
+
+p vetor 
