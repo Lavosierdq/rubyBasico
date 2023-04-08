e1b766356193ba5aec63a3df5b710d8863056f2c
Lavosierdq <lavosierbarbosa@gmail.com>
2023-04-07 23:43:43 -0300

    Fazendo algoritmo de datas

---
+++ algoritmos/fase1/calculateAge.rb
@@ -0,0 +1,8 @@
+# https://www.geeksforgeeks.org/program-calculate-age/
+# Programa para calcular a idade
+
+dataNascimento = '07/09/1996'
+dataAtual = '12/07/2017'
+
+diaSplit = dataNascimento.split('/')
+p diaSplit
