f834bd59106e1df6758a560a6ff92d4691a5c3d0
Lavosierdq <lavosierbarbosa@gmail.com>
2023-04-10 23:53:46 -0300

    fazendo algoritmo de calcular ano

--- algoritmos/fase1/calculateAge.rb
+++ algoritmos/fase1/calculateAge.rb
@@ -1,8 +1,18 @@
+# rubocop:disable all
 # https://www.geeksforgeeks.org/program-calculate-age/
 # Programa para calcular a idade
 
 dataNascimento = '07/09/1996'
 dataAtual = '12/07/2017'
 
-diaSplit = dataNascimento.split('/')
-p diaSplit
+dataNascimentoAtt = dataNascimento.split('/')
+dataAtualAtt = dataAtual.split('/')
+p dataAtualAtt
+p dataNascimentoAtt
+puts dataNascimentoAtt 
+
+
+if dataNascimentoAtt[0].to_i < dataAtualAtt[0].to_i
+  puts " data nascimento é menor"
+end
+ 
