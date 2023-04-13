023d40d9db84c5b22bdbc780693b2f39b8b516ec
Lavosierdq <lavosierbarbosa@gmail.com>
2023-04-12 23:42:18 -0300

    algoritmo de calcular ano

--- algoritmos/fase1/calculateAge.rb
+++ algoritmos/fase1/calculateAge.rb
@@ -4,12 +4,19 @@
 
 dataAniversario= '07/09/1996'
 dataAtual = '12/07/2017'
-diaA = dataAniversario.split('/').slice(0)
-mesA = dataAniversario.split('/').slice(1)
-anoA = dataAniversario.split('/').slice(2)
+diaNascimento = dataAniversario.split('/').slice(0)
+mesNascimento = dataAniversario.split('/').slice(1)
+anoNascimento = dataAniversario.split('/').slice(2)
 
 diaAtual = dataAtual.split('/').slice(0)
 mesAtual = dataAtual.split('/').slice(1)
 anoAtual = dataAtual.split('/').slice(2)
 
- 
+numMes = [31, 28, 31, 30, 31, 30, 31,31, 30, 31, 30, 31]
+
+if mesNascimento > mesAtual
+    mesAtual  =  mesAtual - 1
+    diaAtual = diaAtual + numMes[mesNascimento - 1]
+end
+
+
