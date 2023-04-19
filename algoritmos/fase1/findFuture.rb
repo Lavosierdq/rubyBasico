83ece9f1508bdb3f5d59ec6a8889ecadf16728b4
Lavosierdq <lavosierbarbosa@gmail.com>
2023-04-19 09:13:10 -0300

    resolvendo algoritmo findFuture

--- algoritmos/fase1/findFuture.rb
+++ algoritmos/fase1/findFuture.rb
@@ -5,7 +5,25 @@
     matriz fornecida arr[] . Se tal data não existir, imprima “-1” .
 =end
 
-DataArray = ["22/4/1233","1/3/633","23/5/56645","4/12/233"]
-puts DataArray[0]
-arrayData = DataArray[0].split("/")
+dataArray = ["22/4/1233","1/3/633","23/5/56645","4/12/233"]
+queryArray = ["23/03/4345","12/03/2"]
+puts dataArray[0]
+arrayData = dataArray[0].split("/")
 puts arrayData[2]
+n = dataArray.length-1
+i = 0
+j = 0
+
+while i < n do
+  while j < i+1 do
+    data = dataArray[i].split("/")
+    dataAno = data[2]
+    dataAnoAnterior = dataArray[j].split("/")
+    dataAnoAnteriorLista = dataAnoAnterior[2]
+    puts dataAnoAnteriorLista
+    puts dataAno
+    j = j+1
+  end
+  i = i+1
+end
+
