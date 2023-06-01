425941405beaf45756de50c9e64920e263d5c327
Lavosierdq <lavosierbarbosa@gmail.com>
2023-06-01 14:30:54 -0300

    terminando exercicio 1 de Strings

--- aula3-TratamentoStrings/exercicios1.rb
+++ aula3-TratamentoStrings/exercicios1.rb
@@ -5,10 +5,17 @@ puts a
 b = 10
 c = 15
 
-puts " o valor de b é = " + "#{b}" " e o valor de c = " + "#{c}"
+puts "o valor de b é = " + "#{b}" + " e o valor de c = " + "#{c}"
+puts 'o valor de b é = ' + "#{b+10}" + ' e o valor de c =' + "#{c+10}"
 
 a << " legal de aprender"
 puts a
+c = "lavosier"
+c.concat(" é muito")
+
+c << " preto e bonito" 
+c << " e gosta muito de programação"
+puts c
 
 puts '---' * 10
 
@@ -20,4 +27,15 @@ puts b[-4 .. -2]# imprime do caractere da ultima posição e a quantidade
 puts b[/[aeiou](.)\1/]
 puts b[/[aeiou](.)\1/,0]
 puts b[/[aeiou](.)\1/,1]
-
+puts b[/[aeiou](.)\1/,2]
+puts '---' * 10
+puts b["lo"]
+puts b["the"]
+puts '---' * 10
+puts  "graciara".gsub("gra","89")
+puts  "lavosier".sub("er","gra")
+puts '---' * 10
+puts b.capitalize
+puts 'maria'.capitalize
+puts '---' * 10
+puts 'Adriano'.center(10,'123')
