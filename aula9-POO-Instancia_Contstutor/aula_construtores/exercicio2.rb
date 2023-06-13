abf7e3c767032bc2379f018f1abeccc571c00cac
Lavosierdq <lavosierbarbosa@gmail.com>
2023-06-12 23:54:30 -0300

    exercicio de P.O.O

---
+++ aula9-POO-Instancia_Contstutor/aula_construtores/exercicio2.rb
@@ -0,0 +1,10 @@
+#crie um metodo construtor em ruby que determina
+class Pessoa
+  def initialize(nome,sobrenome,local)
+    @nome = nome
+    @sobrenome = sobrenome
+    @local = local
+    puts "nome:#{nome}, sobrenome: #{sobrenome}, local: #{local}"
+  end
+end
+pessoa = Pessoa.new("lavosier","barbosa","ceilandia")
