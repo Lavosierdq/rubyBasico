daeed316b3ef5a860a657998315be60cae323c62
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-10-25 22:53:32 -0300

    fazendo desafios do site hacker rank

--- hackerRank/exemplo.rb
+++ hackerRank/exemplo.rb
@@ -1,25 +1,13 @@
-def scoring(array)
-  # iterate through each of the element in array using *each* and call update_score on it
-  array.each do |user|
-    update_score(user)
-  end
-end
-
-def update_score(user)
-  puts 'atualizando'
-end
-
-
-
-arr = [0,1,2,3,4,5,6,7,8,9]
-
-scoring(arr)
-
-
-solução...
-def scoring(array)
-    # iterate through each of the element in array using *each* and call update_score on it
-    array.each do |u|
-        u.update_score
+def prime?(num)
+  n = 2
+    while n < num-1
+        if num%n == 0
+          return false
+        end 
+        n = n+1
     end
+    true
 end
+
+numero  = 7
+puts prime?(numero)
--- hackerRank/parImparDesafio.rb
+++ hackerRank/parImparDesafio.rb
@@ -1,4 +1,22 @@
 # Your code here
+
+def prime?(num)
+  n = 2
+  if num <=1
+    return false
+  end
+    while n < num-1
+        if num%n == 0
+          return false
+        end 
+        n = n+1
+    end
+    true
+end
+
+puts prime?(11)
+
+#resposta alternativa
 def prime?(num)
   ((2..num).select { |n| num % n == 0 }).size == 1 ? true  : false
 end
