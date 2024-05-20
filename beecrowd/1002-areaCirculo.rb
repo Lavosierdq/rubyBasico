8c5793bb5d6be083126f7c2e5fa3902fb44ad38a
Lavosier <lavosierbarbosa@gmail.com>
2024-05-19 23:54:03 -0300

    feat: desafios de logica beecrowd

---
+++ beecrowd/1002-areaCirculo.rb
@@ -0,0 +1,9 @@
+# Escreva a sua solução aqui
+# Code your solution here
+# Escriba su solución aquí
+
+n = 3.14159
+raio = gets.chomp.to_f
+
+area = n*raio**2
+printf("A=%2.4f\n",area)
---
+++ beecrowd/1010-calculo-simples.rb
@@ -0,0 +1,12 @@
+cod_peca1 = gets.chomp.to_s
+num_peca1 = gets.chomp.to_s
+valor_peca1 = gets.chomp.to_f
+
+for soma  in 1 .. num_peca1 do
+  soma = soma + valor_peca1
+end
+
+
+cod_peca2 = gets.chomp.to_s
+num_peca2 = gets.chomp.to_s
+valor_peca2 = gets.chomp.to_f
